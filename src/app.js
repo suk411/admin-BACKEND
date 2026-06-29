fc44248cfe7d01062e26f95b9ca2389b9a2dee93
hayat00444 <hayat0004444@gmail.com>
2026-06-29 18:58:37 +0530

    fix: remove all rate limiting from admin apis

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
-import { globalLimiter, authLimiter } from "./utils/rateLimiter.js";
 import adminRoutes from "./routes/admin.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
@@ -32,9 +31,8 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(globalLimiter);
 
-app.use("/api/admin/auth", authLimiter, authRoutes);
+app.use("/api/admin/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 
 app.use((req, res) => {
